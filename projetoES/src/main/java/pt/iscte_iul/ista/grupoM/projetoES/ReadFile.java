8ff68aeeaddedd6ce9b8273b9cf9110b91f84a10
DavidIscte <dsaas@iscte-iul.pt>
2023-11-12 16:57:50 +0000

    Opens Browser

--- projetoES/src/main/java/pt/iscte_iul/ista/grupoM/projetoES/ReadFile.java
+++ projetoES/src/main/java/pt/iscte_iul/ista/grupoM/projetoES/ReadFile.java
@@ -5,13 +5,19 @@ import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVRecord;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.awt.Desktop;
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.io.Reader;
 import java.net.URL;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 
@@ -23,9 +29,23 @@ public class ReadFile {
 		try {
 			records = readCSV(path);
 	        writeTabulatorHTML(records);
+	        String htmlPath = System.getProperty("user.dir") + File.separator + "output.html";
+	        try {
+				openBrowser(new File(htmlPath).toURI());
+			} catch (URISyntaxException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+		}
+	}
+	
+	private void openBrowser(URI uri) throws IOException, URISyntaxException {
+		Desktop desktop = Desktop.isDesktopSupported() ? Desktop.getDesktop() : null;
+		if (desktop != null && desktop.isSupported(Desktop.Action.BROWSE)) {
+			desktop.browse(uri);
 		}
 	}
 	
