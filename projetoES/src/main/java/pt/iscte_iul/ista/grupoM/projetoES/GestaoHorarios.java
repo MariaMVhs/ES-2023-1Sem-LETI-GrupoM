2b55e888de01e920799bab49a21fc8c203bd36dc
lrske25 <lrske@iscte-iul.pt>
2023-12-07 23:11:52 +0000

    Alterações às classes SALA, SALAS e GESTAOHORARIOS

--- projetoES/src/main/java/pt/iscte_iul/ista/grupoM/projetoES/GestaoHorarios.java
+++ projetoES/src/main/java/pt/iscte_iul/ista/grupoM/projetoES/GestaoHorarios.java
@@ -1,5 +1,7 @@
 package pt.iscte_iul.ista.grupoM.projetoES;
 
+import java.util.List;
+
 public class GestaoHorarios {
 
 	public static void main(String[] args) {
@@ -9,5 +11,9 @@ public class GestaoHorarios {
 		Horario_old horario_iscte = new Horario_old();
 		UserInterface window = new UserInterface();
 	
+		List<Sala> salas = salas_iscte.readSalas("CaracterizaçãoDasSalas.csv");
+		System.out.println("Número de salas existentes: " + salas.size());
 	}
+	
+	
 }
