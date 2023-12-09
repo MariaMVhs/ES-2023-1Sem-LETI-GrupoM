57d7f1fa5389f75a6afe7e878acbe093e164e095
MariaMVhs <mmvso@iscte-iul.pt>
2023-12-09 00:17:21 +0000

    tirei extras do salasTest

--- projetoES/src/test/java/pt/iscte_iul/ista/grupoM/projetoES/SalasTest.java
+++ projetoES/src/test/java/pt/iscte_iul/ista/grupoM/projetoES/SalasTest.java
@@ -14,22 +14,6 @@ import java.io.File;
 
 class SalasTest {
 
-	@BeforeAll
-	static void setUpBeforeClass() throws Exception {
-	}
-
-	@AfterAll
-	static void tearDownAfterClass() throws Exception {
-	}
-
-	@BeforeEach
-	void setUp() throws Exception {
-	}
-
-	@AfterEach
-	void tearDown() throws Exception {
-	}
-
 	@Test
 	void testReadSalas() {
 		File testFile = new File("CaracterizaçãoDasSalas.csv");
