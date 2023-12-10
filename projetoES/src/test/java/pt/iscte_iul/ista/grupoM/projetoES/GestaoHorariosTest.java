a9fc138227a11311242bce93f9563d5d4f63c2ef
MariaMVhs <mmvso@iscte-iul.pt>
2023-12-10 15:07:45 +0000

    Update GestaoHorarios

--- projetoES/src/test/java/pt/iscte_iul/ista/grupoM/projetoES/GestaoHorariosTest.java
+++ projetoES/src/test/java/pt/iscte_iul/ista/grupoM/projetoES/GestaoHorariosTest.java
@@ -1,11 +1,5 @@
 package pt.iscte_iul.ista.grupoM.projetoES;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class GestaoHorariosTest {
@@ -13,11 +7,13 @@ class GestaoHorariosTest {
 	@Test
 	final void testMain() {
 		UserInterface window = new UserInterface();
+		Salas salas= new Salas();
 		Horario horario_iscte = new Horario();
 		window.setHorario(horario_iscte);
 		window.window_readSalas();
 		window.window_readHorario();
 		window.start();
+		window.setSalas(salas);
+	    
 	}
-
 }
